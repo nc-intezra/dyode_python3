2f700881094ad73841f95461ed09c99ea1cc1c02
nc-intezra <nick.carter@intezra.com>
2026-09-25 17:05:13 -0400

    Delete tests directory

--- tests/fakes/pymodbus/__init__.py
+++
@@ -1,2 +0,0 @@
-class ModbusDeviceIdentification:
-    pass
--- tests/fakes/pymodbus/server.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-started = []
-
-async def StartAsyncTcpServer(context, identity=None, address=None):
-    started.append(address)
-    await asyncio.Event().wait()
